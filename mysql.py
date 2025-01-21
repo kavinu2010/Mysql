6a375fc33909e6eda377c5f590848989665c6263
kavitha <kavinu2010@gmail.com>
2025-01-21 07:54:05 +0100

    have groupby syntax added

--- mysql.py
+++ mysql.py
@@ -176,9 +176,23 @@ SELECT column_name(s) FROM table2;
 UNION ALL
 SELECT column_name(s) FROM table2;
 
+** SELECT column_name(s)
+FROM table_name
+WHERE condition
+GROUP BY column_name(s)
+ORDER BY column_name(s);
+
+** SELECT column_name(s)
+FROM table_name
+WHERE condition
+GROUP BY column_name(s)
+HAVING condition
+ORDER BY column_name(s);
 
 
-
-
+** SELECT column_name(s)
+FROM table_name
+WHERE EXISTS
+(SELECT column_name FROM table_name WHERE condition);
 
 '''
